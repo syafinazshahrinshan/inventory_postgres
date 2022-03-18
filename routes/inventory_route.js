b90b1d09ffeb7d87a2809840a8d998d26080820d
syafinazshahrinshan <89062054+syafinazshahrinshan@users.noreply.github.com>
2022-03-18 21:50:37 +0800

    Delete inventory_route.js

--- routes/inventory_route.js
+++
@@ -1,6 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-// const inventoryController = require('../controllers/inventory');
-
-// router.post('/inventory', inventoryController.postInventory);
-// module.exports = router;
