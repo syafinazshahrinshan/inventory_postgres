9d2d9524dfadbb235dc7302de0cf87c549b69c64
KiV1 <zaki.jst@gmail.com>
2022-03-29 14:55:08 +0800

    Update db.js ready for deployment

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const Pool = require('pg').Pool
 const pool = new Pool({
     user: process.env.DB_USERNAME || 'superuser' ,
-    password: '1234',
+    password: process.env.DB_PASSWORD || '1234',
     database:'inventory_database',
     host: process.env.DB_HOST || 'localhost',
     port:5432
