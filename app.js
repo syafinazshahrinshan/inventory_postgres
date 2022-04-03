791115b411b349a68b572f59f2b997610816d8d1
KiV1 <zaki.jst@gmail.com>
2022-04-03 14:50:07 +0800

    Update seeding logic

--- app.js
+++ app.js
@@ -130,6 +130,12 @@ app.get('/', function(req, res) {
 app.listen(3050, async() => {
     try{
         await seed.query(`CREATE DATABASE inventory_database;`);
+        
+    }catch(err){
+        console.log(err)
+    }
+    
+    try{
         await pool.query(`CREATE TABLE inventory(
             itemID SERIAL PRIMARY KEY,
             name VARCHAR(255),
@@ -138,26 +144,32 @@ app.listen(3050, async() => {
             pricePerItem DOUBLE PRECISION NOT NULL,
             url VARCHAR(255),
             createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        );`);
+        );`);        
     }catch(err){
         console.log(err)
     }
 
-    const allProducts = await pool.query("SELECT * FROM inventory")
-    //Seeding data
-    if(allProducts.rowCount==0){    
-        await pool.query(`INSERT INTO public.inventory (name,quantity,isavailable,priceperitem,url) VALUES
-        ('Elowyn Sweetheart Knit Top',229,true,36.9,'https://media.lovebonito.com/media/catalog/product/t/h/th2120-031-1_1oajgoomos6tmirg.jpg?width=960&height=1344'),
-        ('Mayven Classic Button Down Shirt',107,true,39.9,'https://media.lovebonito.com/media/catalog/product/t/h/th1374-031_86vfy4q9pjntzkrq.jpg?width=960&height=1344'),
-        ('Sarah Button Down Cardigan',63,true,41.9,'https://media.lovebonito.com/media/catalog/product/t/h/th1714-047_14.jpg?width=960&height=1344'),
-        ('Kasey Tiered Crinkled Dress',2,true,53.9,'https://media.lovebonito.com/media/catalog/product/t/h/th1826-031_r7vqrhw36yapilo3.jpg?width=960&height=1344'),
-        ('Morie High Waisted Straight Leg Pants',74,true,53.9,'https://media.lovebonito.com/media/catalog/product/h/y/hy3589-047-1_rj5wqkcw1bxcfawh.jpg?width=960&height=1344'),
-        ('Roxanne Tailored Peg Leg Pants',61,true,49.9,'https://media.lovebonito.com/media/catalog/product/t/h/th1813-031_ze7hzx1cddciwwd5.jpg?width=960&height=1344'),
-        ('Arianne Tie Front Dress',339,true,49.9,'https://media.lovebonito.com/media/catalog/product/t/h/th2096-261-1_7sznoafdiphxz5qw.jpg?width=960&height=1344'),
-        ('Wyetta Tailored Cape Blazer',92,true,65.9,'https://media.lovebonito.com/media/catalog/product/h/y/hy5180-031.jpg?width=960&height=1344'),
-        ('Glynne Jersey Sweetheart Tie Back Top',26,true,41.9,'https://media.lovebonito.com/media/catalog/product/t/h/th1913-014-1_cxukjxt3u1ztfisj.jpg?width=960&height=1344'),
-        ('Amita Tailored Shorts',43,true,39.9,'https://media.lovebonito.com/media/catalog/product/t/h/th1192-031_fkcrrvdlhnzbtfz4.jpg?width=960&height=1344'),
-        ('Keyla Cupro Twist Front Top',102,true,36.9,'https://media.lovebonito.com/media/catalog/product/l/n/ln0884-052-1_gxfzqmsltx74juia.jpg?width=960&height=1344');`)
+    try{
+        const allProducts = await pool.query("SELECT * FROM inventory")
+        //Seeding data
+        if(allProducts.rowCount==0){    
+            await pool.query(`INSERT INTO public.inventory (name,quantity,isavailable,priceperitem,url) VALUES
+            ('Elowyn Sweetheart Knit Top',229,true,36.9,'https://media.lovebonito.com/media/catalog/product/t/h/th2120-031-1_1oajgoomos6tmirg.jpg?width=960&height=1344'),
+            ('Mayven Classic Button Down Shirt',107,true,39.9,'https://media.lovebonito.com/media/catalog/product/t/h/th1374-031_86vfy4q9pjntzkrq.jpg?width=960&height=1344'),
+            ('Sarah Button Down Cardigan',63,true,41.9,'https://media.lovebonito.com/media/catalog/product/t/h/th1714-047_14.jpg?width=960&height=1344'),
+            ('Kasey Tiered Crinkled Dress',2,true,53.9,'https://media.lovebonito.com/media/catalog/product/t/h/th1826-031_r7vqrhw36yapilo3.jpg?width=960&height=1344'),
+            ('Morie High Waisted Straight Leg Pants',74,true,53.9,'https://media.lovebonito.com/media/catalog/product/h/y/hy3589-047-1_rj5wqkcw1bxcfawh.jpg?width=960&height=1344'),
+            ('Roxanne Tailored Peg Leg Pants',61,true,49.9,'https://media.lovebonito.com/media/catalog/product/t/h/th1813-031_ze7hzx1cddciwwd5.jpg?width=960&height=1344'),
+            ('Arianne Tie Front Dress',339,true,49.9,'https://media.lovebonito.com/media/catalog/product/t/h/th2096-261-1_7sznoafdiphxz5qw.jpg?width=960&height=1344'),
+            ('Wyetta Tailored Cape Blazer',92,true,65.9,'https://media.lovebonito.com/media/catalog/product/h/y/hy5180-031.jpg?width=960&height=1344'),
+            ('Glynne Jersey Sweetheart Tie Back Top',26,true,41.9,'https://media.lovebonito.com/media/catalog/product/t/h/th1913-014-1_cxukjxt3u1ztfisj.jpg?width=960&height=1344'),
+            ('Amita Tailored Shorts',43,true,39.9,'https://media.lovebonito.com/media/catalog/product/t/h/th1192-031_fkcrrvdlhnzbtfz4.jpg?width=960&height=1344'),
+            ('Keyla Cupro Twist Front Top',102,true,36.9,'https://media.lovebonito.com/media/catalog/product/l/n/ln0884-052-1_gxfzqmsltx74juia.jpg?width=960&height=1344');`)
+        }     
+    }catch(err){
+        console.log(err)
     }
+
+
     console.log('Server Started');
 });
